ae48ecd6833df372125e52755c07dd4370d36ced
1679hhl <3298146326@qq.com>
2025-09-29 08:45:57 +0800

    杂项

--- My project/Assets/NewMonoBehaviourScript.cs
+++ My project/Assets/NewMonoBehaviourScript.cs
@@ -2,22 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class ThirtyThree : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void Probability()
     {
+        int n = 6; // 初始概率1/6
 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-    
-    public void MyMethod()
-    {
-        Debug.Log("MyMethod was called!");
+        while (n >= 1)
+        {
+            int r = Random.Range(1, n + 1); // 生成1~n的随机数（包含n）
+            if (r == 1)
+            {
+                Debug.Log("中奖！概率为 1/" + n);
+                break;
+            }
+            else
+            {
+                Debug.Log("未中，概率为 1/" + n);
+                n--;
+            }
+        }
     }
 }
